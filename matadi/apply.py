d121b096ab810dc3461d858baac7aeb9d85aefbd
Andreas D <a.dutzler@gmail.com>
2021-09-21 16:47:06 +0200

    enhance apply method

--- matadi/apply.py
+++ matadi/apply.py
@@ -1,12 +1,11 @@
-from functools import partial
 import numpy as np
-import casadi as ca
 
 
-def apply(x, fun, fun_shape, trailing_axes=2):
+def apply(x, fun, x_shape, fun_shape):
     "Helper function for the calculation of fun(x)."
 
     # get shape of trailing axes
+    trailing_axes = [len(y.shape) - len(y_shape) for y, y_shape in zip(x, x_shape)][0]
     ax = x[0].shape[-trailing_axes:]
 
     def rshape(z):
@@ -21,47 +20,7 @@ def apply(x, fun, fun_shape, trailing_axes=2):
 
     # map function `N` times on reshaped input
     N = np.product(ax)
-    out = np.array(fun.map(N)(*y))
+    out = fun.map(N)(*y)
 
     # return 'i,j,...' reshaped output
-    return out.reshape(*fun_shape, *ax, order="F")
-
-
-class FunctionScalar:
-    def __init__(self, x, fun, *args, **kwargs):
-        
-        self.x = x
-        self.fun = fun
-        
-        self.args = args
-        self.kwargs = kwargs
-        
-        self._f = self.fun(self.x, *self.args, **self.kwargs)
-        
-        self._h = []
-        self._g = []
-        
-        for y in self.x:
-            _h, _g = ca.hessian(self._f, y)
-            self._h.append(_h)
-            self._g.append(_g)
-        
-        # generate casADi function objects
-        self._grad = ca.Function("g", self.x, self._g)
-        self._hessian = ca.Function("h", self.x, self._h)
-    
-    def grad(self, x):
-        ij = x[0].shape[:2]
-        return apply(x, fun=self._grad, fun_shape=ij)
-    
-    def hessian(self, x):
-        ij = x[0].shape[:2]
-        return apply(x, fun=self._hessian, fun_shape=(*ij, *ij))
-
-x = ca.SX.sym("x", 3, 3)
-f = lambda x: ca.trace(x[0])
-
-W = FunctionScalar([x], f)
-
-y = np.random.rand(3,3,5,100)
-W.grad(y)
+    return [np.array(o).reshape(*f, *ax, order="F") for o, f in zip(out, fun_shape)]
