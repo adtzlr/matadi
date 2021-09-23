1a4052f32400b4ff7b59447bbc6ba607b5c08339
Andreas D <a.dutzler@gmail.com>
2021-09-23 20:06:43 +0200

    add hgo model

--- matadi/models/_anisotropic_hyperelasticity.py
+++ matadi/models/_anisotropic_hyperelasticity.py
@@ -1,7 +1,7 @@
-from ..math import transpose, det, DM, cos, sin, pi, sqrt, if_else
+from ..math import transpose, det, DM, exp, cos, sin, pi, sqrt, if_else
 
 
-def fiber(F, E, k=1, angle=30, axis=2, compression=False):
+def fiber(F, E, angle, k=1, axis=2, compression=False):
 
     a = angle * pi / 180
     plane = [(1, 2), (2, 0), (0, 1)][axis]
@@ -21,9 +21,49 @@ def fiber(F, E, k=1, angle=30, axis=2, compression=False):
     return E * strain ** 2 / 2
 
 
-def fiber_family(F, E, k=1, angle=30, axis=2, compression=False):
+def fiber_family(F, E, angle, k=1, axis=2, compression=False):
 
     f1 = fiber(F, E, k, angle, axis, compression)
     f2 = fiber(F, E, k, -angle, axis, compression)
 
     return f1 + f2
+
+
+def holzapfel_gasser_ogden(F, c, k1, k2, angle, axis=2):
+
+    C = transpose(F) @ F
+    J1, J2, J3 = invariants(C)
+
+    Cu = J3 ** (-1 / 3) * C
+
+    I1 = J3 ** (-1 / 3) * J1
+    # I2 = J3 ** (-2 / 3) * J2
+
+    alpha = angle * pi / 180
+    plane = [(1, 2), (2, 0), (0, 1)][axis]
+
+    N1 = DM.zeros(3)
+    N1[plane, :] = DM([cos(alpha), sin(alpha)])
+
+    N2 = DM.zeros(3)
+    N2[plane, :] = DM([cos(alpha), -sin(alpha)])
+
+    A1 = N1 @ transpose(N1)
+    A2 = N2 @ transpose(N2)
+
+    I4 = trace(Cu @ A1)
+    I6 = trace(Cu @ A2)
+
+    # I5 = trace(C @ C @ A1)
+    # I7 = trace(C @ C @ A2)
+
+    # I8 = (transpose(N1) @ N2) * transpose(N1) @ C @ N2
+
+    W_iso = c / 2 * (I1 - 3)
+
+    w1 = exp(k2 * (I4 - 1) ** 2) - 1
+    w2 = exp(k2 * (I6 - 1) ** 2) - 1
+
+    W_aniso = k1 / (2 * k2) * (w1 + w2)
+
+    return W_iso + W_aniso
