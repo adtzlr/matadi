d438d59abd45aed21eb5cbf4c902f5a390e2502d
Andreas D <a.dutzler@gmail.com>
2021-09-23 10:55:45 +0200

    Update math.py
    fix eigvals, add invariants, add more imports from casadi

--- matadi/math.py
+++ matadi/math.py
@@ -10,7 +10,6 @@ from casadi import (
     cross,
     times,
     eig_symbolic,
-    eig_symbolic as eigvals,
     ldl,
     # trig
     sin,
@@ -30,4 +29,31 @@ from casadi import (
     sum1,
     sum2,
     sumsqr,
+    pi,
+    fabs,
+    linspace,
+    #
+    if_else,
+    SX,
+    DM,
+    MX,
 )
+
+
+def invariants(T):
+
+    I1 = trace(T)
+    I2 = (I1 ** 2 - trace(T @ T)) / 2
+    I3 = det(T)
+
+    return I1, I2, I3
+
+
+def eigvals(T, eps=1e-5):
+
+    T[0, 0] += eps
+    T[1, 1] -= eps
+
+    wT = eig_symbolic(T)
+
+    return wT
