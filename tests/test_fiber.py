e486f6f64bac3bc3b966ebc4b5ea2768b6a4b78e
Andreas D <a.dutzler@gmail.com>
2021-09-24 10:31:51 +0200

    update tests

--- tests/test_fiber.py
+++ tests/test_fiber.py
@@ -22,18 +22,8 @@ def test_fiber():
         DW = M.hessian([FF])
 
         assert W0[0].shape == (2,)
-        assert dW[0].shape == (
-            3,
-            3,
-            2,
-        )
-        assert DW[0].shape == (
-            3,
-            3,
-            3,
-            3,
-            2,
-        )
+        assert dW[0].shape == (3, 3, 2,)
+        assert DW[0].shape == (3, 3, 3, 3, 2,)
 
 
 def test_hgo():
@@ -55,18 +45,8 @@ def test_hgo():
         DW = M.hessian([FF])
 
         assert W0[0].shape == (2,)
-        assert dW[0].shape == (
-            3,
-            3,
-            2,
-        )
-        assert DW[0].shape == (
-            3,
-            3,
-            3,
-            3,
-            2,
-        )
+        assert dW[0].shape == (3, 3, 2,)
+        assert DW[0].shape == (3, 3, 3, 3, 2,)
 
 
 if __name__ == "__main__":
