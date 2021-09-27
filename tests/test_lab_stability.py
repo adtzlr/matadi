8c4a2011ffec22091df1ff57361d1209e459af05
Andreas D <a.dutzler@gmail.com>
2021-09-27 15:58:06 +0200

    try incompressible stretch_3 if root method fails
    simplify hessian (don't use shear terms in ux,bx,ps)

--- tests/test_lab_stability.py
+++ tests/test_lab_stability.py
@@ -4,12 +4,21 @@ from matadi.models import mooney_rivlin
 import matplotlib.pyplot as plt
 
 
-def test_lab():
+def test_lab_stab():
 
-    mat = MaterialHyperelastic(mooney_rivlin, C10=0.5, C01=0.5, bulk=5000.0,)
+    mat = MaterialHyperelastic(mooney_rivlin, C10=0.0, C01=0.5, bulk=2000.0,)
 
     lab = Lab(mat)
-    data = lab.run(ux=True, bx=True, ps=True, num=50)
+    data = lab.run(ux=True, bx=True, ps=True, num=50, stretch_max=2.5)
+
+    fig, ax = lab.plot(data, stability=True)
+    plt.close(fig)
+
+    # dW and DW are always lists...
+    assert len(data[0].stress) == 50
+    assert len(data[0].stretch) == 50
+
+    data = lab.run(ux=True, bx=True, ps=True, num=50, stretch_min=0.1, stretch_max=1.0)
 
     fig, ax = lab.plot(data, stability=True)
     # plt.close(fig)
@@ -20,4 +29,4 @@ def test_lab():
 
 
 if __name__ == "__main__":
-    test_lab()
+    test_lab_stab()
