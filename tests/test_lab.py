22345e109b1574e59e326a8b8426930a5d366a18
Andreas D <a.dutzler@gmail.com>
2021-09-28 08:59:27 +0200

    Update test_lab.py

--- tests/test_lab.py
+++ tests/test_lab.py
@@ -1,9 +1,32 @@
 from matadi import MaterialHyperelastic, Lab
+import matadi.models as md
 from matadi.models import neo_hooke, extended_tube, van_der_waals, mooney_rivlin
 
 import matplotlib.pyplot as plt
 
-from test_models import library
+
+def library():
+    "Library with models and parameters."
+
+    database = {
+        md.saint_venant_kirchhoff: {"mu": 1.0, "lmbda": 20.0},
+        md.neo_hooke: {"C10": 0.5},
+        md.mooney_rivlin: {"C10": 0.3, "C01": 0.8},
+        md.yeoh: {"C10": 0.5, "C20": 0.1, "C30": 0.01},
+        md.third_order_deformation: {
+            "C10": 0.3,
+            "C01": 0.2,
+            "C11": 0.02,
+            "C20": -0.1,
+            "C30": 0.02,
+        },
+        md.ogden: {"mu": (1.0, 0.2), "alpha": (2.0, -1.5)},
+        md.arruda_boyce: {"C1": 1.0, "limit": 3.2},
+        md.extended_tube: {"Gc": 0.1867, "Ge": 0.2169, "beta": 0.2, "delta": 0.09693},
+        md.van_der_waals: {"mu": 1.0, "beta": 0.1, "a": 0.5, "limit": 5.0},
+    }
+
+    return database
 
 
 def pre(
