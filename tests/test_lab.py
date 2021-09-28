7c342d5757bb08ef99d26fdcbf3923e989e65ea1
Andreas D <a.dutzler@gmail.com>
2021-09-28 08:55:10 +0200

    Update test_lab.py

--- tests/test_lab.py
+++ tests/test_lab.py
@@ -1,32 +1,70 @@
 from matadi import MaterialHyperelastic, Lab
-from matadi.models import neo_hooke
+from matadi.models import neo_hooke, extended_tube, van_der_waals, mooney_rivlin
 
 import matplotlib.pyplot as plt
+
+from test_models import library
+
+
+def pre(
+    model,
+    num=20,
+    test_without_bulk=False,
+    stability=False,
+    plot=False,
+    close=True,
+    run_all=False,
+    run_kwargs={},
+):
+
+    lib = library()
+    kwargs = lib[model]
+
+    if test_without_bulk:
+        # init material without bulk modulus
+        mat = MaterialHyperelastic(model, **kwargs)
+
+    # init material with bulk modulus
+    mat = MaterialHyperelastic(model, **kwargs, bulk=5000.0)
+
+    # init lab
+    lab = Lab(mat)
+
+    # run experiments
+    if run_all:
+        data = lab.run(ux=False, bx=False, ps=False, num=num, **run_kwargs)
+        data = lab.run(ux=True, bx=False, ps=False, num=num, **run_kwargs)
+        data = lab.run(ux=True, bx=True, ps=False, num=num, **run_kwargs)
+
+    data = lab.run(ux=True, bx=True, ps=True, num=num, **run_kwargs)
+
+    if plot:
+        # plot stress vs. stretch
+        fig, ax = lab.plot(data, stability=stability)
+
+        if close:
+            plt.close(fig)
+
+    # dW and DW are lists
+    assert len(data[0].stress) == num
+    assert len(data[0].stretch) == num
+
+    return data
 
 
 def test_lab():
 
-    # test material without bulk modulus
-    mat = MaterialHyperelastic(neo_hooke, C10=0.5)
+    data = pre(neo_hooke, test_without_bulk=True)
+    data = pre(neo_hooke, run_all=True)
 
-    mat = MaterialHyperelastic(
-        neo_hooke,
-        C10=0.5,
-        bulk=5000.0,
-    )
+    run_kwargs = {"stretch_min": 0.1, "stretch_max": 1.0}
 
-    lab = Lab(mat)
-    data = lab.run(ux=False, bx=False, ps=False, num=20)
-    data = lab.run(ux=True, bx=False, ps=False, num=20)
-    data = lab.run(ux=True, bx=True, ps=False, num=20)
-    data = lab.run(ux=True, bx=True, ps=True, num=20)
-    fig, ax = lab.plot(data)
+    data = pre(neo_hooke, run_kwargs=run_kwargs)
+    data = pre(extended_tube)
+    data = pre(van_der_waals)
+    data = pre(mooney_rivlin, close=True, stability=True, plot=True)
 
-    plt.close(fig)
-
-    # dW and DW are always lists...
-    assert len(data[0].stress) == 20
-    assert len(data[0].stretch) == 20
+    del data
 
 
 if __name__ == "__main__":
