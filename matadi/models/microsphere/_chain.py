2097ca4192aa932a961481e878189be7c41a1b31
Andreas D <a.dutzler@gmail.com>
2021-11-10 10:33:25 +0100

    fix langevin chain model
    see #37

--- matadi/models/microsphere/_chain.py
+++ matadi/models/microsphere/_chain.py
@@ -1,11 +1,28 @@
-from ...math import sqrt, atanh
+from ...math import log, sinh, sqrt
 
 
 def langevin(stretch, mu, N):
-    """Langevin model (Padé approximation) given by the free energy
-    of a single chain as a function of the stretch."""
+    """Langevin model given by the free energy of a single chain as a function
+    of the stretch (assuming a complex valued logarithm). The inverse
+    Langevin function is defined by a Padé approximation."""
 
-    return mu * (stretch + 2 * sqrt(N) * atanh(stretch / sqrt(N)))
+    x = stretch / sqrt(N)
+    L = x * (3 - x ** 2) / (1 - x ** 2)
+
+    return mu * N * (x * L + log(L / sinh(L)))
+
+
+def langevin2(stretch, mu, N):
+    """Langevin model given by the free energy of a single chain as a function
+    of the stretch (assuming a complex valued logarithm). The inverse Langevin
+    function is defined by a Padé approximation.
+
+    **Note**: This function is optimized for fast gradient evaluation but
+    cannot be used to calculate the actual free energy - only its derivative
+    w.r.t. the stretch gives a real-valued result for the region of interest
+    `N > stretch ** 2`."""
+
+    return mu * (stretch ** 2 / 2 - N * log(stretch ** 2 - N))
 
 
 def gauss(stretch, mu):
