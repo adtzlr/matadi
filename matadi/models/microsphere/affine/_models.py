5527022b4b27b59b1cbf470d0b54919f9119a9d5
Andreas D <a.dutzler@gmail.com>
2021-11-13 21:40:26 +0100

    set default quadrature for micro-sphere

--- matadi/models/microsphere/affine/_models.py
+++ matadi/models/microsphere/affine/_models.py
@@ -1,9 +1,10 @@
 from ..._helpers import isochoric_volumetric_split
 from ....math import transpose, sum1, diag, sqrt, inv, det
+from ..quadrature._bazant_oh import BazantOh
 
 
 @isochoric_volumetric_split
-def microsphere_affine_stretch(F, quadrature, f, kwargs):
+def microsphere_affine_stretch(F, f, kwargs, quadrature=BazantOh(n=21)):
     "Micro-sphere model: Non-affine stretch part."
 
     r = quadrature.points
@@ -16,7 +17,7 @@ def microsphere_affine_stretch(F, quadrature, f, kwargs):
 
 
 @isochoric_volumetric_split
-def microsphere_affine_tube(F, quadrature, f, kwargs):
+def microsphere_affine_tube(F, f, kwargs, quadrature=BazantOh(n=21)):
     "Micro-sphere model: Non-affine tube part."
 
     r = quadrature.points
