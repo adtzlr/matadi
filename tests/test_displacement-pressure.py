765f2c4d750075c879ccc6b3e9170e1182548928
Andreas D <a.dutzler@gmail.com>
2022-03-13 12:40:46 +0100

    test u/p with state variables

--- tests/test_displacement-pressure.py
+++ tests/test_displacement-pressure.py
@@ -8,26 +8,36 @@ from matadi.math import det, dev, inv
 
 def test_up():
 
+    # deformation gradient
     F = Variable("F", 3, 3)
+    
+    # state variables
+    z = Variable("z", 5, 9)
 
     @displacement_pressure_split
     def fun(x):
-        F = x[0]
+        F, z = x[0], x[-1]
         C = F.T @ F
-        return det(F) ** (-2 / 3) * dev(C) @ inv(C)
+        return det(F) ** (-2 / 3) * dev(C) @ inv(C), z
+    
+    # get pressure variable from augmented function
+    p = fun.p
 
-    M = MaterialTensor([F, fun.p], fun, triu=True)
+    # Materail as a function of `F` and `p`
+    # with additional state variables `z`
+    M = MaterialTensor([F, p, z], fun, triu=True, statevars=1)
 
     FF = np.random.rand(3, 3, 8, 100)
     pp = np.random.rand(1, 8, 100)
+    zz = np.random.rand(5, 9, 8, 100)
 
     for a in range(3):
         FF[a, a] += 1
 
-    P = M.function([FF, pp])
-    A = M.gradient([FF, pp])
+    P, constraint, statevars_new = M.function([FF, pp, zz])
+    A = M.gradient([FF, pp, zz])
 
-    assert len(P) == 2
+    assert len(P) == 3
     assert len(A) == 3
 
 
