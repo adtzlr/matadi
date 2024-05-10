f0c91a3c49e901649e1e57ed864917c44be21984
Sad-Abd <abedisadjad@gmail.com>
2024-05-10 21:48:59 +0330

    Added finite strain viscoelasticity with Mooney-Rivlin

--- src/matadi/models/_viscoelasticity.py
+++ src/matadi/models/_viscoelasticity.py
@@ -1,4 +1,4 @@
-from ..math import astensor, asvoigt, det, gradient, inv, trace
+from ..math import astensor, asvoigt, det, gradient, inv, trace, eye, unimodular, sqrtm
 
 
 def finite_strain_viscoelastic(x, mu, eta, dtime):
@@ -16,3 +16,35 @@ def finite_strain_viscoelastic(x, mu, eta, dtime):
 
     # first Piola-Kirchhoff stress tensor and state variable
     return gradient(mu / 2 * (I1 - 3), F), asvoigt(Ci)
+
+
+def finite_strain_viscoelastic_MR(x, c10, c01, eta, dtime):
+    """
+    Finite strain viscoelastic material formulation with Mooney-Rivlin hyperelasticity
+    (Shutov 2018) https://doi.org/10.1002/nme.5724
+    """
+
+    # Split input into the deformation gradient and the vector of state variables
+    F, Cin = x[0], x[-1]
+
+    # Right cauchy-green deformation tensor
+    C = F.T @ F
+
+    # Based on <<TABLE 1: Iteration-free Euler backward method on the reference configuration>>
+    A = (
+        unimodular(sqrtm(inv(C)))
+        @ (astensor(Cin) + (dtime / eta) * c10 * unimodular(C))
+        @ unimodular(sqrtm(inv(C)))
+    )
+    eps = c01 * (dtime / eta)
+    phi0 = det(A) ** (1 / 3)
+    phi = phi0 - (trace(A) / (3 * phi0)) * eps
+    X = 2 * A @ inv(sqrtm(phi * phi * eye(3) + 4 * eps * A) + phi * eye(3))
+
+    Ci = unimodular(sqrtm(C) @ X @ sqrtm(C))
+
+    I1 = trace(unimodular(C @ inv(Ci)))
+    I2 = trace(unimodular(Ci @ inv(C)))
+
+    # First Piola-Kirchhoff stress tensor and state variable
+    return gradient(c10 / 2 * (I1 - 3) + c01 / 2 * (I2 - 3), F), asvoigt(Ci)
